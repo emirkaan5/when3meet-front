91653f642cf91798c2bebb6793b496f49f90e775
Chunderwave <charlottechoi@icloud.com>
2025-10-10 11:34:52 -0400

    testing_server

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express();
+const user_routes = require("./routes/user_routes.js")
 
+const app = express();
 app.use(express.json());
+
 app.use('/api/events',       require('./routes/event_routes'));
-app.use('/api/users',        require('./routes/user_routes'));
+app.use('/api/users',        user_routes);
 app.use('/api/availability', require('./routes/availability_routes'));
+
 
 module.exports = app;
--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const userCtrl = require("../controllers/user_controller");
+
 router.post("/", userCtrl.createUser);
+router.get("/",(req,res)=>{
+    res.status(200).send("connected successfully");
+});
 
 module.exports = router;
