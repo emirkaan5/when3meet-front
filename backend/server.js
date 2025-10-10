1b1d9860876f9c37dee12ccebba56cf35095747d
Chunderwave <charlottechoi@icloud.com>
2025-10-10 11:58:44 -0400

    comparing with working

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI;
 
-(async () => {
-  await connectDB(MONGO_URI);
-  app.listen(PORT, () => {
-    console.log(`🚀 Server listening at http://localhost:${PORT}`);
-  });
-})();
+app.get("/sth", (req, res) => {
+  res.send("You are connected.");
+});
+
+connectDB(MONGO_URI);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening at http://localhost:${PORT}`);
+});
