e7900954734029d15fae81eed380be180a65b773
Adriana Caraeni <149904373+AdrianaCaraeni@users.noreply.github.com>
2025-11-13 09:32:25 -0500

    Add MeetingSummary route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
 import Availability from './pages/Availability.jsx'
 import CreateEvent from './pages/CreateEvent.jsx'
+import MeetingSummary from './pages/MeetingSummary.jsx' 
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/availability" element={<Availability />} />
       <Route path="/create" element={<CreateEvent />} />
+      <Route path="/event/:eventId/summary" element={<MeetingSummary />} /> {/* After meeting sceduled have this page*/}
       <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   )
