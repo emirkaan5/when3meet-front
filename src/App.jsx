59459966f6ac36acb489128d2a0d137703164027
Unmesh Reza <unmesh17780@gmail.com>
2025-11-13 11:16:41 -0500

    Fixed some naming issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
 import Availability from './pages/Availability.jsx'
 import CreateEvent from './pages/CreateEvent.jsx'
-import MeetingSummary from './pages/MeetingSummary.jsx' 
+import SaveMeeting from './pages/SaveMeeting.jsx'
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/availability" element={<Availability />} />
       <Route path="/create" element={<CreateEvent />} />
-      <Route path="/event/:eventId/summary" element={<MeetingSummary />} /> {/* After meeting sceduled have this page*/}
+      <Route path="/event/:eventId/summary" element={<SaveMeeting />} /> {/* After meeting sceduled have this page*/}
       <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   )
