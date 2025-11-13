d333f3f955e6200d23c8ff5572ddab3effdb012e
Adriana Caraeni <149904373+AdrianaCaraeni@users.noreply.github.com>
2025-11-13 09:28:44 -0500

    Add new routes for finalizing and summarizing meetings to event-routes

--- backend/routes/event_routes.js
+++ backend/routes/event_routes.js
@@ -8,4 +8,9 @@ router.get('/:eventId', ctrl.getEvent);       // GET  /api/events/:eventId
 router.patch('/:eventId', ctrl.updateEvent);  // PATCH /api/events/:eventId
 router.delete('/:eventId', ctrl.deleteEvent); // DELETE /api/events/:eventId
 
+
+// NEW ROUTES
+router.post('/:eventId/finalize', ctrl.finalizeMeeting);
+router.get('/:eventId/summary', ctrl.getMeetingSummary);
+
 module.exports = router;
